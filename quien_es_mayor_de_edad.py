d4bfb73488a6b5ff51324e8d6b19c45d34532de6
KmiiloBerrio <kmiiloberrio@gmail.com>
2020-04-30 19:59:51 -0500

    Quien es myor de edad, reto deprogramacion ramificada

---
+++ quien_es_mayor_de_edad.py
@@ -0,0 +1,21 @@
+user_one = input('Hola usuario 1 como es tu nombre? : ')
+age_user_one = int(input(f'{user_one} cual es tu edad? : '))
+user_two = input('Hola usuario 2 como es tu nombre? : ')
+age_user_one = int(input(f'{user_two} cual es tu edad? : '))
+
+def is_user_one_mayor_que_user_two():
+    return age_user_one > age_user_two
+
+def is_user_one_menor_que_user_two():
+    return age_user_one < age_user_two
+
+def print_older_user_message(old_user, young_user):
+    print(f'Ey {old_user} eres mayor que {young_user}')
+
+if is_user_one_mayor_que_user_two:
+    print_older_user_message(user_one, user_two)
+elif is_user_one_menor_que_user_two:
+    print_older_user_message(user_two, user_one)
+else:
+    print(f'Ey {user_one} y {user_two} ustedes tienen la misma edad!')
+

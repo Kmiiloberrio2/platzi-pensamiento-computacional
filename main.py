ebf8190c907fa3334e16cff6cef2a287ee5f285a
KmiiloBerrio <kmiiloberrio@gmail.com>
2020-05-07 20:59:53 -0500

    se crea archivo main para probar los algoritmos de raiz cuadrada

---
+++ main.py
@@ -0,0 +1,29 @@
+import aproximacion
+import busqueda_binaria
+import enumeracion
+
+
+def switch_algoritm(argument):
+    switcher = {
+        1: busqueda_binaria.calculate_square_root,
+        2: enumeracion.calculate_square_root,
+        3: aproximacion.calculate_square_root,
+    }
+    func = switcher.get(argument, lambda: 'Invalid Number')
+    return func(int(input('Escoge un numero: ')))
+
+
+def main():
+    algoritm = int(input('''Que algoritmo deseas utilizar? 
+                            1: busqueda_binaria,
+                            2: enumeracion,
+                            3: aproximacion,
+                            4: iteraciones,
+                            5: programas_ramificados,
+                            6: quien_es_mayor_de_edad
+                            (Elije una opcion, presionando solo un numero): '''))
+    switch_algoritm(algoritm)
+
+
+if __name__ == '__main__':
+    main()

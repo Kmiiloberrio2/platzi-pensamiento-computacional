c70e41db1a97b9e583cdc7dbd1b4cb73a48e6cfc
KmiiloBerrio <kmiiloberrio@gmail.com>
2020-05-11 22:13:34 -0500

    example docstring google style

---
+++ example_docstring.py
@@ -0,0 +1,10 @@
+def suma(a, b):
+    """Suma dos valores
+       
+       Args:
+            a (int): Cualquier entero
+            b (int): Cualquier entero
+       Returns:
+            int: Resultado de la suma.
+    """
+    return a + b
